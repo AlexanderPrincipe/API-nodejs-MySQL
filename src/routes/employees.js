8992878967c5169bd40b8c9fa088b4ab4136d3da
Alexander <principetolentinoa@hotmail.com>
2020-01-13 00:00:18 -0500

    API

---
+++ src/routes/employees.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+
+const mysqlConnection = require('../database');
+
+router.get('/', (req, res) => {
+    mysqlConnection.query('SELECT * FROM cliente', (err, rows, fields) => {
+        if(!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.get('/:codigo', (req, res) => {
+    const { codigo } = req.params;
+    mysqlConnection.query('Select * from cliente WHERE codigo = ?', [codigo], (err, rows, fields) => {
+        if(!err) {
+            res.json(rows[0]);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
+
+module.exports = router;
